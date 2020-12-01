11d5952667cbbdbd7fcf4aac409fc10cb97daeef
Nidhin BM <nidhinbm.bm@gmail.com>
2020-12-01 13:00:40 +0530

    CORS error

--- index.js
+++ index.js
@@ -42,7 +42,11 @@ app.get("/", (req, res) => {
     res.send(html);
     
   });
-
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.post('/signup', function(req, res){
   const password = req.body.password
   let  hashedPassword  =  bcrypt.hashSync(password, 8);
