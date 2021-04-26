f476158ae6d594d7f7dc3bda45097f74c47dd368
Nidhin BM <nidhinbm.bm@gmail.com>
2021-04-26 16:33:49 +0530

    date

--- index.js
+++ index.js
@@ -243,36 +243,9 @@ app.get('/v2/notification', async (req, res, next) => {
 
 app.post("/v2/log",async function(req, res, next) {
   let d = new Date();
-  let nz_date_string = new Date().toLocaleString("en-US", { timeZone: "Asia/Calcutta" });
+  let ind = new Date().toLocaleString("en-US", { timeZone: "Asia/Calcutta" });
 
-// Date object initialized from the above datetime string
-let date_nz = new Date(nz_date_string);
 
-// year as (YYYY) format
-let year = date_nz.getFullYear();
-
-// month as (MM) format
-let month = ("0" + (date_nz.getMonth() + 1)).slice(-2);
-
-// date as (DD) format
-let date = ("0" + date_nz.getDate()).slice(-2);
-
-// hours as (HH) format
-let hours = ("0" + date_nz.getHours()).slice(-2);
-
-// minutes as (mm) format
-let minutes = ("0" + date_nz.getMinutes()).slice(-2);
-
-// seconds as (ss) format
-let seconds = ("0" + date_nz.getSeconds()).slice(-2);
-
-// date as YYYY-MM-DD format
-let date_yyyy_mm_dd = year + "-" + month + "-" + date;
-// time as hh:mm:ss format
-let time_hh_mm_ss = hours + ":" + minutes + ":" + seconds;
-// date and time as YYYY-MM-DD hh:mm:ss format
-
-let date_time = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
 
   await log.put({
     key:req.body.slug+ "-"+ req.body.userId ,
@@ -285,7 +258,7 @@ let date_time = year + "-" + month + "-" + date + " " + hours + ":" + minutes + 
     slug: req.body.slug,
     cover: req.body.cover,
     time: d.getTime(),
-    timeStamp=date_time
+    timeStamp=ind
     
  })
  res.send({status :"done"})
